99f5f558ece8bbb1c238153413d9b14a3055d443
bharat1999 <bharatmalik1999@gmail.com>
2021-05-15 11:37:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import react from 'react'
+import {useEffect,useState} from 'react'
+import DataDisplay from './components/dataDisplay'
+
+
 
 function App() {
-  return (
+  const [hdata,setHdata] = useState([])
+  useEffect(()=>{
+  const fetchData =  async ()=> {await axios.get("https://api.npoint.io/4d61424b0910b4a2b692")
+    .then(function(res){
+      setHdata(res.data.hospitals)
+      console.log(hdata[0].phoneNumber)
+    })  
+    .catch(function(e){
+      console.log(e)
+    })
+  }
+fetchData()
+})
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Isolation Beds</h1>
+      </div>
+      {hdata.length!==0?
+        hdata.map((h)=>{
+          return <DataDisplay name={h.name} add={h.location.formattedAddress} no={h.phoneNumber[0]}
+            link={h.location.link} total={h.general.total} vacant={h.general.available} occupied={h.general.occupied}
+          />
+        })  
+        :<div>Loader</div>}
     </div>
   );
 }
---
+++ src/components/dataDisplay.js
@@ -0,0 +1,48 @@
+import "./dataDisplay.css"
+import React from 'react';
+
+
+
+
+
+function dataDisplay(props) {
+    return(
+        <div className="container data_box">
+            <div className="row">
+                <div className="col col-9 text-start">
+                    <p>{props.name}</p>
+                    <p>{props.type}</p>
+                    <p>{props.add}</p>
+                    <p>{props.no}</p> 
+                    <a href={props.link}>Map</a>
+                </div>
+                <div className="col col-3">
+                    <div className="row">
+                        <div className="col col-4">
+                            <p>Total</p>
+                        </div>
+                        <div className="col col-4">
+                            <p className="vacant">Vacant</p>
+                        </div>
+                        <div className="col col-4">
+                            <p className="occupied">Occupied</p>
+                        </div> 
+                    </div>    
+                    <div className="row">
+                        <div className="col col-4">
+                            <p>{props.total}</p>
+                        </div>
+                        <div className="col col-4">
+                            <p className="vacant">{props.vacant}</p>
+                        </div>
+                        <div className="col col-4">
+                            <p className="occupied">{props.occupied}</p>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default dataDisplay;
